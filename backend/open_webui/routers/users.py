b0496f278c215f608991e24319060ebcb48c1a56
Jesper Kristensen <cableman@linuxdev.dk>
2025-05-15 16:00:04 +0200

    4356: Added new permission to disable adv chat settings

--- backend/open_webui/routers/users.py
+++ backend/open_webui/routers/users.py
@@ -133,6 +133,7 @@ class SharingPermissions(BaseModel):
 
 class ChatPermissions(BaseModel):
     controls: bool = True
+    advanced_controls: bool = True
     file_upload: bool = True
     delete: bool = True
     edit: bool = True
