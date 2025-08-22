3302e6fcab3d10aa3b1b7460f1edc5b9122e810c
Sine Jespersen <sinejespersen@gmail.com>
2025-08-22 11:16:20 +0200

    5234: add two helper functions, as config seem to be loaded in a different way

--- src/routes/was/+page.svelte
+++ src/routes/was/+page.svelte
@@ -4,10 +4,28 @@
 	import { goto } from '$app/navigation';
 	import { config } from '$lib/stores';
 
+	function IsJson(str) {
+		try {
+			JSON.parse(str);
+		} catch (e) {
+			return false;
+		}
+		return true;
+	}
+
+	function getUrl(str) {
+		if (IsJson(str)) {
+			return JSON.parse(str);
+		} else {
+			return str;
+		}
+	}
+
 	onMount(async () => {
 		const WAS_REDIRECT_URL = $config?.extended_features?.was_redirect_url || false;
-		if (WAS_REDIRECT_URL) window.open(JSON.parse(WAS_REDIRECT_URL), '_blank');
+		if (WAS_REDIRECT_URL) window.open(getUrl(WAS_REDIRECT_URL), '_blank');
 		goto('/');
 	});
 </script>
+
 <!-- /PATCH REDIRECT TO WAS -->
